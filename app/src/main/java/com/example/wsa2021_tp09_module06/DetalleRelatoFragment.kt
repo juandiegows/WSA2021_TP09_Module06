0d85e4467b17fb655a93a30ac1b76897d8fa1158
juandiegows <mejiamaestrejuandiego@gmail.com>
2022-09-29 19:28:26 -0500

    ultimo cambios

--- app/src/main/java/com/example/wsa2021_tp09_module06/DetalleRelatoFragment.kt
+++ app/src/main/java/com/example/wsa2021_tp09_module06/DetalleRelatoFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.observe
 
 class DetalleRelatoFragment : Fragment() {
 
@@ -26,6 +27,11 @@ class DetalleRelatoFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(DetalleRelatoViewModel::class.java)
         // TODO: Use the ViewModel
+
+        viewModel.relato.observe(viewLifecycleOwner){
+
+        }
+        viewModel.GetRelato()
     }
 
 }
--- app/src/main/java/com/example/wsa2021_tp09_module06/DetalleRelatoViewModel.kt
+++ app/src/main/java/com/example/wsa2021_tp09_module06/DetalleRelatoViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.wsa2021_tp09_module06
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.wsa2021_tp09_module06.helper.Singleton
+import com.example.wsa2021_tp09_module06.model.Relatos
 
 class DetalleRelatoViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var _relato = MutableLiveData(Singleton.relatos)
+    var relato: LiveData<Relatos> = _relato
+
+    fun GetRelato(){
+        _relato.postValue(Singleton.relatos)
+    }
 }
